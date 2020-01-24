d4b0360f0641fdd75b10455d7d03ca3e5082dec5
cristiano oliveira <cristianoliveira063@hotmail.com>
2020-01-23 23:51:35 -0400

    Adicionando JPA e configurando o Data Source

---
+++ src/main/java/br/com/askfood/AskfoodApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.askfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskfoodApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AskfoodApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/askfood/AskfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.askfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskfoodApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
