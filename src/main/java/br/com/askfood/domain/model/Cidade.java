f884567d5b9dea1eb3ac60777f8a75875c1d12e5
cristiano oliveira <cristianoliveira063@hotmail.com>
2020-01-24 23:37:03 -0400

    #1 - Criação das entidades Cidade,Cozinha,Estado,FormaPagamento,Permissao,Restaurante

---
+++ src/main/java/br/com/askfood/domain/model/Cidade.java
@@ -0,0 +1,27 @@
+package br.com.askfood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.*;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Cidade {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Estado estado;
+
+}
---
+++ src/main/java/br/com/askfood/domain/model/Restaurante.java
@@ -0,0 +1,27 @@
+package br.com.askfood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Restaurante {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private BigDecimal taxaFrete;
+
+    @ManyToOne
+    @JoinColumn(name = "cozinha_id")
+    private Cozinha cozinha;
+
+}
