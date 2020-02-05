25e337c58dd563d8ca87ed42d8093b6c15bf1c0f
cristiano oliveira <cristianoliveira063@hotmail.com>
2020-02-04 23:52:16 -0400

    #3 - Criação  camada de serviços e recursos  de cozinha e estado

--- src/main/java/br/com/askfood/api/controller/EstadoController.java
+++ src/main/java/br/com/askfood/api/controller/EstadoController.java
@@ -1,11 +1,14 @@
 package br.com.askfood.api.controller;
 
+import br.com.askfood.domain.exception.EntidadeEmUsoException;
+import br.com.askfood.domain.exception.EntidadeNaoEncontradaException;
 import br.com.askfood.domain.model.Estado;
 import br.com.askfood.domain.repository.EstadoRepository;
+import br.com.askfood.domain.service.CadastroEstadoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,8 +19,50 @@ public class EstadoController {
     @Autowired
     private EstadoRepository  estadoRepository;
 
+    @Autowired
+    private CadastroEstadoService cadastroEstadoService;
+
     @GetMapping
     public List<Estado> listar(){
         return  estadoRepository.findAll();
     }
+
+    @GetMapping("/{estadoId}")
+    public ResponseEntity<Estado> buscar(@PathVariable Long estadoId) {
+        Estado estado = estadoRepository.findById(estadoId).orElse(null);
+
+        if (estado != null) {
+            return ResponseEntity.ok(estado);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{estadoId}")
+    public ResponseEntity<?> remover(@PathVariable Long estadoId) {
+        try {
+            cadastroEstadoService.excluir(estadoId);
+            return ResponseEntity.noContent().build();
+
+        } catch (EntidadeNaoEncontradaException e) {
+            return ResponseEntity.notFound().build();
+
+        } catch (EntidadeEmUsoException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body(e.getMessage());
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
---
+++ src/main/java/br/com/askfood/domain/service/CadastroEstadoService.java
@@ -0,0 +1,40 @@
+package br.com.askfood.domain.service;
+
+import br.com.askfood.domain.exception.EntidadeEmUsoException;
+import br.com.askfood.domain.exception.EntidadeNaoEncontradaException;
+import br.com.askfood.domain.model.Estado;
+import br.com.askfood.domain.repository.EstadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CadastroEstadoService {
+
+    @Autowired
+    private EstadoRepository estadoRepository;
+
+    public Estado salvar(Estado estado) {
+        return estadoRepository.save(estado);
+    }
+
+    public void excluir(Long estadoId) {
+        try {
+            estadoRepository.deleteById(estadoId);
+
+        } catch (EmptyResultDataAccessException e) {
+            throw new EntidadeNaoEncontradaException(
+                    String.format("Não existe um cadastro de estado com código %d", estadoId));
+
+        } catch (DataIntegrityViolationException e) {
+            throw new EntidadeEmUsoException(
+                    String.format("Estado de código %d não pode ser removido, pois está em uso", estadoId));
+        }
+    }
+
+
+
+
+
+}
