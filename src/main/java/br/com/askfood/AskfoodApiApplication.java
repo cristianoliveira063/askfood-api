b83e0b3fec182224ca1ef30a5048faa240374aed
cristiano oliveira <cristianoliveira063@hotmail.com>
2020-02-09 11:48:07 -0400

    #5 - Criação  camada de repositorio generico e ajustes nas pesquisas de restaurantes

--- src/main/java/br/com/askfood/AskfoodApiApplication.java
+++ src/main/java/br/com/askfood/AskfoodApiApplication.java
@@ -1,9 +1,12 @@
 package br.com.askfood;
 
+import br.com.askfood.infraestruture.repository.CustomJpaRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
 public class AskfoodApiApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/br/com/askfood/infraestruture/repository/CustomJpaRepositoryImpl.java
@@ -0,0 +1,36 @@
+package br.com.askfood.infraestruture.repository;
+
+import java.util.Optional;
+
+import javax.persistence.EntityManager;
+
+import br.com.askfood.domain.repository.CustomJpaRepository;
+import org.springframework.data.jpa.repository.support.JpaEntityInformation;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+
+
+@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+public class CustomJpaRepositoryImpl<T, ID> extends SimpleJpaRepository<T, ID>
+        implements CustomJpaRepository<T, ID> {
+
+    private EntityManager manager;
+
+    public CustomJpaRepositoryImpl(JpaEntityInformation<T, ?> entityInformation,
+                                   EntityManager entityManager) {
+        super(entityInformation, entityManager);
+
+        this.manager = entityManager;
+    }
+
+    @Override
+    public Optional<T> buscarPrimeiro() {
+        var jpql = "from " + getDomainClass().getName();
+
+        T entity = manager.createQuery(jpql, getDomainClass())
+                .setMaxResults(1)
+                .getSingleResult();
+
+        return Optional.ofNullable(entity);
+    }
+
+}
