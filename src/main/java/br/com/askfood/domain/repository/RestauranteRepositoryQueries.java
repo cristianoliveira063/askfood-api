bd34d15e7df60f54adde57c54bd9f49df47f1007
cristiano oliveira <cristianoliveira063@hotmail.com>
2020-02-09 11:48:07 -0400

    #5 - Criação  camada de repositorio generico e ajustes nas pesquisas de restaurantes

---
+++ src/main/java/br/com/askfood/domain/repository/RestauranteRepositoryQueries.java
@@ -0,0 +1,14 @@
+package br.com.askfood.domain.repository;
+
+import br.com.askfood.domain.model.Restaurante;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface RestauranteRepositoryQueries {
+
+    List<Restaurante> find(String nome,
+                           BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal);
+
+    List<Restaurante> findComFreteGratis(String nome);
+}
---
+++ src/main/java/br/com/askfood/infraestruture/repository/spec/RestauranteSpecs.java
@@ -0,0 +1,19 @@
+package br.com.askfood.infraestruture.repository.spec;
+
+import br.com.askfood.domain.model.Restaurante;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.math.BigDecimal;
+
+public class RestauranteSpecs {
+
+    public static Specification<Restaurante> comFreteGratis() {
+        return (root, query, builder) ->
+                builder.equal(root.get("taxaFrete"), BigDecimal.ZERO);
+    }
+
+    public static Specification<Restaurante> comNomeSemelhante(String nome) {
+        return (root, query, builder) ->
+                builder.like(root.get("nome"), "%" + nome + "%");
+    }
+}
