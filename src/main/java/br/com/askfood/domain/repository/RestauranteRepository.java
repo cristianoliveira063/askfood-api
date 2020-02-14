ac68a761fd29c23112fee4c85379d0f3da302d01
cristiano oliveira <cristianoliveira063@hotmail.com>
2020-02-14 00:16:20 -0400

    #7 - Criar consulta customizada de restaurantes para evitar lentidão

--- src/main/java/br/com/askfood/domain/repository/RestauranteRepository.java
+++ src/main/java/br/com/askfood/domain/repository/RestauranteRepository.java
@@ -2,6 +2,7 @@ package br.com.askfood.domain.repository;
 
 import br.com.askfood.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -13,6 +14,13 @@ import java.util.Optional;
 public interface RestauranteRepository
         extends CustomJpaRepository<Restaurante, Long>, RestauranteRepositoryQueries,
         JpaSpecificationExecutor<Restaurante> {
+
+    // Errata: se um restaurante não tiver nenhuma forma de pagamento associada a ele,
+    // esse restaurante não será retornado usando JOIN FETCH r.formasPagamento.
+    // Para resolver isso, temos que usar LEFT JOIN FETCH r.formasPagamento
+//	@Query("from Restaurante r join fetch r.cozinha join fetch r.formasPagamento")
+    @Query("from Restaurante r join fetch r.cozinha left join fetch r.formasPagamento")
+    List<Restaurante> findAll();
 
     List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 
