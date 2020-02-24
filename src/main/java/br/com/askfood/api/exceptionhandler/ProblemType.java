822e6f09fadb85d029035931ac53e9de406ca736
cristiano oliveira <cristianoliveira063@hotmail.com>
2020-02-23 23:48:02 -0400

    #11 - Habilitando erros na desserialização de propriedades inexistentes ou ignoradas

--- src/main/java/br/com/askfood/api/exceptionhandler/ProblemType.java
+++ src/main/java/br/com/askfood/api/exceptionhandler/ProblemType.java
@@ -4,7 +4,11 @@ import lombok.Getter;
 
 @Getter
 public enum ProblemType {
-    ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada");
+
+    MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível"),
+    ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada"),
+    ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
+    ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio");
 
     private String title;
     private String uri;
