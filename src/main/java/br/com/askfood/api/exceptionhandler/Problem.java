487b3ebcfaad3decf2f8ee6399b660e5d5a2e970
cristiano oliveira <cristianoliveira063@hotmail.com>
2020-02-25 11:20:54 -0400

    #11 - Tratamento e modelagem de erros da API

--- src/main/java/br/com/askfood/api/exceptionhandler/Problem.java
+++ src/main/java/br/com/askfood/api/exceptionhandler/Problem.java
@@ -5,13 +5,18 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @JsonInclude(Include.NON_NULL)
 @Getter
 @Builder
 public class Problem {
 
     private Integer status;
+    private LocalDateTime timestamp;
     private String type;
     private String title;
     private String detail;
+
+    private String userMessage;
 }
