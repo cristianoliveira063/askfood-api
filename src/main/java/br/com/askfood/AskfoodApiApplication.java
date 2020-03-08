eb02068cc0f7fc7fd91728ee2ec490b80514a9ac
cristiano oliveira <cristianoliveira063@hotmail.com>
2020-03-07 23:42:42 -0400

    #15 - Configurando e refatorando o projeto para usar UTC

--- src/main/java/br/com/askfood/AskfoodApiApplication.java
+++ src/main/java/br/com/askfood/AskfoodApiApplication.java
@@ -5,11 +5,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
 public class AskfoodApiApplication {
 
     public static void main(String[] args) {
+
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+
         SpringApplication.run(AskfoodApiApplication.class, args);
     }
 
