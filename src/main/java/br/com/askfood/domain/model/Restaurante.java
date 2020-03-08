2eb9ef518db512d607cf696d00497d9590d7a9f9
cristiano oliveira <cristianoliveira063@hotmail.com>
2020-03-07 23:42:42 -0400

    #15 - Configurando e refatorando o projeto para usar UTC

--- src/main/java/br/com/askfood/domain/model/Restaurante.java
+++ src/main/java/br/com/askfood/domain/model/Restaurante.java
@@ -15,7 +15,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.groups.ConvertGroup;
 import javax.validation.groups.Default;
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -48,15 +48,13 @@ public class Restaurante {
     @Embedded
     private Endereco endereco;
 
-    @JsonIgnore
     @CreationTimestamp
     @Column(nullable = false, columnDefinition = "datetime")
-    private LocalDateTime dataCadastro;
+    private OffsetDateTime dataCadastro;
 
-    @JsonIgnore
     @UpdateTimestamp
     @Column(nullable = false, columnDefinition = "datetime")
-    private LocalDateTime dataAtualizacao;
+    private OffsetDateTime dataAtualizacao;
 
     @JsonIgnore
     @ManyToMany
